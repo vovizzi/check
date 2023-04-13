0b824477edaa551605f9eb838daca2443c576786
Vladimir <vovaazoov@rambler.ru>
2023-04-13 15:03:45 +0300

    ...

---
+++ kek.js
@@ -0,0 +1,4 @@
+let offsetHeight = document.querySelector('main').offsetHeight;
+console.log('Высота:', offsetHeight);
+let offsetWidth = document.querySelector('main').clientWidth;
+console.log('Ширина:', offsetWidth);
