56deeb8d09332cf03b932349782772da40237205
Vladimir <vovaazoov@rambler.ru>
2023-04-18 17:43:09 +0300

    change

--- check.js
+++ check.js
@@ -2,3 +2,10 @@ let offsetHeight = document.querySelector('main').offsetHeight;
 console.log('Высота:', offsetHeight);
 let offsetWidth = document.querySelector('main').clientWidth;
 console.log('Ширина:', offsetWidth);
+
+let overall_image = document.querySelector('.overall-image')
+
+
+  if (window.innerWidth === 768) {
+    overall_image.remove()
+  }
