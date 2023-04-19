7c6a9a6d9cd88cb9ee9960a6abe581b69572aafc
Vladimir <vovaazoov@rambler.ru>
2023-04-19 17:09:02 +0300

    change

--- check.js
+++ check.js
@@ -3,9 +3,26 @@ console.log('Высота:', offsetHeight);
 let offsetWidth = document.querySelector('main').clientWidth;
 console.log('Ширина:', offsetWidth);
 
-let overall_image = document.querySelector('.overall-image')
+// Переключение кнопки
 
+ let images = document.querySelectorAll('.overall-image-2 img');
+console.log(images)
+ let current = 0;
 
-  if (window.innerWidth === 768) {
-    overall_image.remove()
+ function slider() {
+  for(let i =0; i<images.length; i++) {
+      images[i].classList.add('opacity0');
   }
+    images[current].classList.remove('opacity0');
+    current++;
+  if(current == 2) {
+      current = 0
+  }
+
+ }
+
+ slider()
+//  document.querySelector('.overall-image-2')
+
+let button = document.querySelector('.btn-switch')
+button.onclick = slider
